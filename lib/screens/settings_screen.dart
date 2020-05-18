d43585393ce1b11afefb97ec311ececb03f9ae4d
Fabio Kreis <fabiokreis@gmail.com>
2020-05-18 07:58:09 -0300

    Adde pushReplacementNamed in drawer component

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -7,6 +8,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Configurações'),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('conifig'),
       ),
